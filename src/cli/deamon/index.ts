17b5016ff36815bb6bbb136d960aa876de6990cd
Albert Sanchez <albert.san.iza@gmail.com>
2026-02-21 00:38:02 -0800

    feat: add deamon command to manage Jellyfish daemon

---
+++ src/cli/deamon/index.ts
@@ -0,0 +1,3 @@
+import { Command } from 'commander'
+
+export const deamonCommand = new Command('deamon').description('Manage the Jellyfish daemon')
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { program } from 'commander'
 
 import pkg from '../package.json'
+import { deamonCommand } from './cli/deamon'
 import { helloWorldCommand } from './cli/hello-world'
 import { updateCommand } from './cli/update'
 
 program.name(pkg.name).description(pkg.description).version(pkg.version, '-v, --version')
 
+program.addCommand(deamonCommand)
 program.addCommand(helloWorldCommand)
 program.addCommand(updateCommand)
 
