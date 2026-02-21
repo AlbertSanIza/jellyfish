e78fa5ea166ee8d424f88b13a224c2b237308c6f
Albert Sanchez <albert.san.iza@gmail.com>
2026-02-21 01:21:31 -0800

    Remove hello-world CLI command
    Delete the placeholder CLI command (src/cli/hello-world.ts) and remove its import and registration from src/index.ts. Cleans up a test stub that is no longer needed.

--- src/cli/hello-world.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from 'commander'
-
-export const helloWorldCommand = new Command('hello-world').description('Just somthing to test the CLI').action(() => {
-    console.log('Hello, World!')
-})
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import { program } from 'commander'
 
 import pkg from '../package.json'
 import { daemonCommand } from './cli/daemon'
-import { helloWorldCommand } from './cli/hello-world'
 import { updateCommand } from './cli/update'
 
 program.name(pkg.name).description(pkg.description).version(pkg.version, '-v, --version')
 
 program.addCommand(daemonCommand)
-program.addCommand(helloWorldCommand)
 program.addCommand(updateCommand)
 
 program.parse()
