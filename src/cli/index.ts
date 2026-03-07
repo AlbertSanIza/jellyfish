e8d7a2631ec57c291be849cbec2b4617d3b3fed8
Albert Sanchez <albert.san.iza@gmail.com>
2026-03-07 18:17:31 +0900

    Add telegramCommand and reorder command imports in CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,12 +1,16 @@
 import { program } from 'commander'
 
 import pkg from '../../package.json'
+import { claudeCommand } from './claude'
 import { daemonCommand } from './daemon'
+import { telegramCommand } from './telegram'
 import { updateCommand } from './update'
 
 program.name(pkg.name).description(pkg.description).version(pkg.version, '-v, --version')
 
+program.addCommand(claudeCommand)
 program.addCommand(daemonCommand)
+program.addCommand(telegramCommand)
 program.addCommand(updateCommand)
 
 program.parse()
